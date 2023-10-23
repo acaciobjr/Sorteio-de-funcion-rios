d17f301b00315adcb9239717b5e4b5b145343504
Acácio <99909819+acaciobjr@users.noreply.github.com>
2023-10-22 21:52:41 -0300

    Update Randint.py

--- Randint.py
+++ Randint.py
@@ -1,29 +1,20 @@
-# Sorteio-de-funcion-rios
-sorteio simples de funcionários com RandINT
-
 from random import randint
-print('precisamos sortear 1 entre os 10 funcionários do RH.')
-sorteio = input("Digite qualquer coisa para iniciar o sorteio:")
 
-num = (randint(1, 10))
+quantidade_total = int(input("Quantidade total de funcionários: "))
+quantidade_sorteio = int(input("Quantidade de pessoas a serem sorteadas: "))
 
-if num == 1:
-    print('o funcionário sorteado foi: marcelo')
-elif num == 2:
-    print('o funcionário sorteado foi: sonia')
-elif num == 3:
-    print('o funcionário sorteado foi: anderessa')
-elif num == 4:
-    print('o funcionário sorteado foi: eliezer')
-elif num == 5:
-    print('o funcionário sorteado foi: roberto')
-elif num == 6:
-    print('o funcionário sorteado foi: angela')
-elif num == 7:
-    print('o funcionário sorteado foi: tereza')
-elif num == 8:
-    print('o funcionário sorteado foi: mateus')
-elif num == 9:
-    print('o funcionario sorteado foi: manoel')
-elif num == 10:
-    print('o funcionario sorteado foi: jorge')
+
+funcionarios = [
+    'marcelo', 'sonia', 'anderessa', 'eliezer', 'roberto',
+    'angela', 'tereza', 'mateus', 'manoel', 'jorge'
+]
+
+if quantidade_sorteio > quantidade_total:
+    print("A quantidade de pessoas a serem sorteadas não pode ser maior que a quantidade total de funcionários.")
+else:
+    # Realiza o sorteio
+    print('Funcionários sorteados:')
+    for _ in range(quantidade_sorteio):
+        num = randint(0, quantidade_total - 1)
+        print(f"O funcionário sorteado foi: {funcionarios[num]}")
+u
